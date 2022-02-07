2b200c1949a0c70761430d77ef8ff8ffc14f1581
Devin <mtaghavinima@gmail.com>
2022-02-07 23:38:19 +0330

    Initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import { LightBox } from 'r-lightbox';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div>dsadsa</div>
+            <LightBox />
+        </div>
+    );
+}
+
+export default App;
